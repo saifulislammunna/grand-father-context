f207aaeb375ccf8035ebbc86c367dfa70df2c457
saifulislammunna <saifulmunna28@gmail.com>
2021-10-02 19:59:37 +0600

    house added

--- src/components/GrandFather/GrandFather.js
+++ src/components/GrandFather/GrandFather.js
@@ -3,10 +3,12 @@ import Aunt from '../Aunt/Aunt';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 
-const GrandFather = () => {
+const GrandFather = (props) => {
+    const {house} = props;
     return (
         <div>
             <h2>GrandFather</h2>
+            <h5>Houise : {house}</h5>
            <div style={{display: 'flex'}}>
            <Father></Father>
            <Uncle></Uncle>
